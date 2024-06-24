00944f19f5e9a3bbed9a1b0c5db67b8243d417fb
eliasZemmouri <elias.zemmouri@student.vinci.be>
2024-06-24 18:13:20 +0200

    transfert

--- lib/components/acceuil/restaurants_page.dart
+++ lib/components/acceuil/restaurants_page.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
-
+import 'package:geolocator/geolocator.dart';
+import 'dart:math' as math;
 import 'category_card.dart';
-
 import '../../data/produit.dart';
 import '../../data/produit_service.dart';
 
@@ -13,6 +13,7 @@ class RestaurantsPage extends StatefulWidget {
 class _RestaurantsPageState extends State<RestaurantsPage> {
   final ProduitService _produitService = ProduitService();
   late List<Produit> _produits = [];
+  late Position _currentPosition;
 
   @override
   void initState() {
@@ -21,15 +22,61 @@ class _RestaurantsPageState extends State<RestaurantsPage> {
   }
 
   Future<void> _loadProduits() async {
-    final produits = await _produitService.fetchAllProduits();
-    print("baaaaka");
-    if (produits.isNotEmpty) {
+    try {
+      _currentPosition = await _getCurrentLocation();
+      final produits = await _produitService.fetchAllProduits();
+      final filteredProduits = produits.where((produit) {
+        final distance = calculateDistance(
+          _currentPosition.latitude,
+          _currentPosition.longitude,
+          produit.latitude,
+          produit.longitude,
+        );
+        return distance <= 5; // Filtrer les produits dans un rayon de 5 km
+      }).toList();
+
       setState(() {
-        _produits = produits;
+        _produits = filteredProduits;
       });
+    } catch (e) {
+      print("Erreur lors du chargement des produits: $e");
     }
   }
 
+  double calculateDistance(double lat1, double lon1, double lat2, double lon2) {
+    const p = 0.017453292519943295; // Pi / 180
+    final c = math.cos;
+    final a = 0.5 - c((lat2 - lat1) * p) / 2 +
+        c(lat1 * p) * c(lat2 * p) *
+            (1 - c((lon2 - lon1) * p)) / 2;
+    return 12742 * math.asin(math.sqrt(a)); // 2 * R * asin(sqrt(a)), R = 6371 km
+  }
+
+  Future<Position> _getCurrentLocation() async {
+    bool serviceEnabled;
+    LocationPermission permission;
+
+    // Vérifier si les services de localisation sont activés
+    serviceEnabled = await Geolocator.isLocationServiceEnabled();
+    if (!serviceEnabled) {
+      return Future.error('Les services de localisation sont désactivés.');
+    }
+
+    permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+      if (permission == LocationPermission.denied) {
+        return Future.error('Les permissions de localisation sont refusées.');
+      }
+    }
+
+    if (permission == LocationPermission.deniedForever) {
+      return Future.error(
+          'Les permissions de localisation sont refusées en permanence.');
+    }
+
+    return await Geolocator.getCurrentPosition();
+  }
 
   @override
   Widget build(BuildContext context) {
--- lib/data/produit_service.dart
+++ lib/data/produit_service.dart
@@ -1,6 +1,8 @@
 import 'dart:convert';
 import 'package:http/http.dart' as http;
 import 'package:user_project/data/produit.dart';
+import 'dart:math' as math;
+import 'package:geolocator/geolocator.dart';
 
 
 class ProduitService {
@@ -16,5 +18,47 @@ class ProduitService {
     } else {
       throw Exception('Failed to load produits');
     }
+  }
+  Future<List<Produit>> fetchFilteredProduits(String type, double latitude, double longitude, double rayon) async {
+    final response = await http.get(Uri.parse('$apiBaseUrl/filtre?type=$type&latitude=$latitude&longitude=$longitude&rayon=$rayon'));
+
+    if (response.statusCode == 200) {
+      List jsonResponse = json.decode(response.body);
+      return jsonResponse.map((produit) => Produit.fromJson(produit)).toList();
+    } else {
+      throw Exception('Failed to load produits');
+    }
+  }
+  double calculateDistance(double lat1, double lon1, double lat2, double lon2) {
+    const p = 0.017453292519943295; // Pi / 180
+    final c = math.cos;
+    final a = 0.5 - c((lat2 - lat1) * p) / 2 +
+        c(lat1 * p) * c(lat2 * p) *
+            (1 - c((lon2 - lon1) * p)) / 2;
+    return 12742 * math.asin(math.sqrt(a)); // 2 * R * asin(sqrt(a)), R = 6371 km
+  }
+  Future<Position> _getCurrentLocation() async {
+    bool serviceEnabled;
+    LocationPermission permission;
+
+    serviceEnabled = await Geolocator.isLocationServiceEnabled();
+    if (!serviceEnabled) {
+      return Future.error('Les services de localisation sont désactivés.');
+    }
+
+    permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+      if (permission == LocationPermission.denied) {
+        return Future.error('Les permissions de localisation sont refusées.');
+      }
+    }
+
+    if (permission == LocationPermission.deniedForever) {
+      return Future.error(
+          'Les permissions de localisation sont refusées en permanence.');
+    }
+
+    return await Geolocator.getCurrentPosition();
   }
 }
